084199de73fb51a0f68a2621a84e82b0bd17a542
pberdaye <pberdaye@LES005446.corp.capgemini.com>
2018-08-07 09:21:17 +0200

    Clase Estudiante creada el martes

---
+++ GitJavaDemo2/src/main/java/com/pablo/modelo/Estudiante.java
@@ -0,0 +1,15 @@
+package com.pablo.modelo;
+
+public class Estudiante {
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
